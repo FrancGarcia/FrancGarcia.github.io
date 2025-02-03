afe8df71f7de9ac00a30138cb059d247f46f5f7f
Francisco Garcia <frgarcia@ucsd.edu>
2025-02-02 22:57:02 -0800

    Website WIP

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,16 +7,29 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('/desktop_pc/scene.gltf')
   return (
     <mesh>
       <hemisphereLight intensity={0.15}
       groundColor="black" />
       <pointLight intensity={1} />
+      <spotLight 
+        position={[-20,50,10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive 
-        object={computer.scene} />
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
+  
 }
 
 const ComputersCanvas = () => {
@@ -39,5 +52,4 @@ const ComputersCanvas = () => {
     </Canvas>
   )
 }
-
 export default Computers
