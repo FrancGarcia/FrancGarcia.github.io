0f57bea61b7be895fb1005c957fe0711776e2eff
Francisco Garcia <frgarcia@ucsd.edu>
2025-02-02 21:56:45 -0800

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Projects, StarsCanvas} from './components'
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,6 +13,7 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
+        <Projects />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
